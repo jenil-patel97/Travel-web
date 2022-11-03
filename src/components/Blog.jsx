861c9d8b7ab5377c65818372c403eefd378a2bcc
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-03 16:45:38 +0530

    fix : Blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Prague from "../assets/prague.jpg";
 import Lemur from "../assets/lemur.jpg";
+import Colombia from "../assets/colombia.jpg";
 
 const Blog = () => {
   return (
-    <div className="w-full mx-auto md:px-[8rem] md:py-16 px-4 py-2 bg-black md:h-[220vh] h-[300vh]">
+    <div className="w-full mx-auto md:px-[8rem] md:py-16 px-4 py-2 bg-black md:h-[330vh] h-[440vh]">
       <div>
         <p className="text-white font-dance text-center md:text-4xl text-2xl mt-4 cursor-default underline">
           Blog
@@ -25,10 +26,10 @@ const Blog = () => {
             travelers disappointed by their visits to Prague because of the
             city's tourist schlock and crowds. Yes, there's a fair heap of that.
             But, there are also ways to avoid it and much more to see and
-            experience in Prague without crowd. That's what this insider's
-            guide is all about with tourist traps to avoid, best things to do,
-            favorite beers and pubs, neighborhoods to stay in, and where
-            to eat in Prague .
+            experience in Prague without crowd. That's what this insider's guide
+            is all about with tourist traps to avoid, best things to do,
+            favorite beers and pubs, neighborhoods to stay in, and where to eat
+            in Prague .
           </p>
           <button className="text-black bg-white px-12 py-2 rounded-md md:ml-[11rem] ml-[7rem] mt-5 font-semibold">
             Read
@@ -62,6 +63,34 @@ const Blog = () => {
             slash-and-burn agriculture and desertification remain huge problems.
           </p>
           <button className="text-black bg-white px-12 py-2 rounded-md md:ml-[11rem] ml-[7rem] mt-5 font-semibold">
+            Read
+          </button>
+        </div>
+      </div>
+      <p className="text-white font-dance text-center md:text-4xl text-2xl md:mt-[6rem] mt-16 cursor-default">
+        Travel to Colombia : First Impressions
+      </p>
+      <div className="md:grid grid-cols-2">
+        <div>
+          <img
+            className="mt-[5rem] h-[28rem] w-[32rem]"
+            src={Colombia}
+            alt="/"
+          />
+        </div>
+        <div>
+          <p className="text-white font-dance text-center mt-[8rem] text-xl cursor-default">
+            While I looked forward to our visit to Colombia, I harbored the
+            occasional image of thuggy bush-mustached Colombian narco-gangsters
+            and aggressive gold cap-toothed street thieves shaking me down in
+            the back shadows of Bogota or Medellin. (Yes, I realize I’ve
+            probably watched one too many bad airplane movies.) Colombia,
+            thankfully, was altogether different. We spent time on our own,
+            under the auspices of friends, on tours, in cities, way up in the
+            hills, on the coast, and in destinations in between. No
+            narco-gangsters. No untoward experiences, for us.
+          </p>
+          <button className="text-black bg-white px-12 py-2 rounded-md md:ml-[11rem] ml-[7rem] mt-8 font-semibold">
             Read
           </button>
         </div>
