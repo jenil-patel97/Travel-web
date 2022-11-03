ceb8cb101dc26140a944b6beeeb150856a324969
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-03 16:23:12 +0530

    fix: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,13 +12,15 @@ const Footer = () => {
     <div className="w-full bg-gray-300 py-16">
       <div className="max-w-[1240px] mx-auto flex flex-col px-4">
         <div className="sm:flex text-center justify-center items-center">
-          <h1 className="font-dance text-3xl md:mr-[50rem] cursor-default">Travel</h1>
+          <h1 className="font-dance text-3xl md:mr-[50rem] cursor-default">
+            Travel
+          </h1>
           <div className="flex justify-between w-full sm:max-w-[280px] my-4">
-            <FaFacebook size={25}/>
-            <FaTwitter size={25}/>
-            <FaInstagram size={25}/>
-            <FaPinterest size={25}/>
-            <FaYoutube size={25}/>
+            <FaFacebook size={25} />
+            <FaTwitter size={25} />
+            <FaInstagram size={25} />
+            <FaPinterest size={25} />
+            <FaYoutube size={25} />
           </div>
         </div>
         <div className="flex justify-between">
@@ -36,7 +38,9 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <p className="text-center text-sm font-dance cursor-default md:text-xl font-bold md:mt-12 mt-4">© 2022 Travel by Jenil.  All Rights reserved.</p>
+      <p className="text-center text-sm font-dance cursor-default md:text-xl font-bold md:mt-12 mt-4">
+        © 2022 Travel by Jenil. All Rights reserved.
+      </p>
     </div>
   );
 };
